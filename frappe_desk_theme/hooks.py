e85d901dfe20060bcfacecf1b45d945f0af60b40
astaa <astaa@DESKTOP-U8GKOJ7.localdomain>
2026-05-08 16:37:47 +0600

    udpated sidebar and icon

--- frappe_desk_theme/hooks.py
+++ frappe_desk_theme/hooks.py
@@ -24,17 +24,17 @@ app_license = "mit"
 # Includes in <head>
 # ------------------
 # include js, css files in header of desk.html
-app_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css?v=1.1.0"
+app_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css?v=1.6.0"
 # app_include_js = "/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js"
 
 app_include_js = [
-	"/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js?v=1.1.0",
-	"/assets/frappe_desk_theme/js/sidebar/sidebar_override.bundle.js?v=1.1.0",
-	"/assets/frappe_desk_theme/js/sidebar/breadcrumb_override_patch.js?v=1.1.0",
+	"/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js?v=1.6.0",
+	"/assets/frappe_desk_theme/js/sidebar/sidebar_override.bundle.js?v=1.6.0",
+	"/assets/frappe_desk_theme/js/sidebar/breadcrumb_override_patch.js?v=1.6.0",
 ]
 # include js, css files in header of web template
-web_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css?v=1.1.0"
-web_include_js = "/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js?v=1.1.0"
+web_include_css = "/assets/frappe_desk_theme/css/frappe_desk_theme.bundle.css?v=1.6.0"
+web_include_js = "/assets/frappe_desk_theme/js/frappe_desk_theme.bundle.js?v=1.6.0"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "frappe_desk_theme/public/scss/website"
